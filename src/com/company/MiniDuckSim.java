60f1888575a00351faaa3c4a167d1a1796043045
Rune Svendsen <rune2381@eamv.dk>
2021-04-17 18:32:34 +0200

    Test and first commit + push

---
+++ src/com/company/MiniDuckSim.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class MiniDuckSim {
+    public static void main(String [] args){
+        Duck mallard = new MallardDuck();
+        mallard.display();
+        mallard.performFly();
+        mallard.performQuack();
+
+        Duck model = new ModelDuck();
+        model.display();
+        model.performFly();
+        model.setFlyBehavior(new FlyRocketPowered());
+        model.performFly();
+
+        Duck redhead = new RedheadDuck();
+        redhead.display();
+        redhead.performFly();
+        redhead.performQuack();
+    }
+}
---
+++ src/com/company/RedheadDuck.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class RedheadDuck extends Duck{
+    public RedheadDuck(){
+        flyBehavior = new FlyWithWings();
+        quackBehavior = new Quack();
+
+        }
+
+        public void display(){
+            System.out.println("I'm a redhead Duck!");
+        }
+}
